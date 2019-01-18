96dc67127f266bd8f8a8f7fbbb7e8017b5c39f38
Thiago <thiagorthomaz@gmail.com>
2019-01-17 22:27:38 -0200

    Add Movie Genre

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -11,7 +11,8 @@ namespace Vidly.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
-
+        public byte GenreId { get; set; }
+        public Genre Genre { get; set; }
 
     }
 }
