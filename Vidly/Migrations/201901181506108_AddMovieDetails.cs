509b5667bac6dc1315874ae3870509521deb193e
Thiago Thomaz <thiagorthomaz@gmail.com>
2019-01-18 13:19:18 -0200

    Movie details

---
+++ Vidly/Migrations/201901181506108_AddMovieDetails.cs
@@ -0,0 +1,22 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddMovieDetails : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "ReleaseDate", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Movies", "DateAdded", c => c.DateTime(nullable: false));
+            AddColumn("dbo.Movies", "Stock", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Movies", "Stock");
+            DropColumn("dbo.Movies", "DateAdded");
+            DropColumn("dbo.Movies", "ReleaseDate");
+        }
+    }
+}
