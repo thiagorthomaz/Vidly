56f7365262f0f224c7c8cfa6c5f77cbc3426bacb
Thiago <thiagorthomaz@gmail.com>
2019-01-20 18:55:54 -0200

    Changed Movie labels

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,10 +12,18 @@ namespace Vidly.Models
 
         public int Id { get; set; }
         public string Name { get; set; }
+
+        [Required]
+        [Display(Name= "Genre")]
         public byte GenreId { get; set; }
+
         public Genre Genre { get; set; }
+
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         public DateTime DateAdded { get; set; }
+
+        [Display(Name = "Number in stock")]
         public int Stock { get; set; }
 
 
