0abac470da21cfaed83d6478bdf682aec83dd7c9
Thiago <thiagorthomaz@gmail.com>
2019-01-23 21:39:00 -0200

    Implemented some customer's validation and Validation Anti Forgery Token

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -16,8 +16,8 @@ namespace Vidly
                         "~/Scripts/jquery-3.2.1.js",
                         "~/Scripts/popper.js"));
 
-            /*bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));*/
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -31,6 +31,7 @@ namespace Vidly.Controllers
 
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipType
             };
 
@@ -55,6 +56,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer) {
 
             if (!ModelState.IsValid) {
